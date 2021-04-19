0a646f961f72f53dd94f68513f33cee221122902
Luis Barata da Fonseca Magalhaes/IT/CSUK <Luis.Magalhaes@creditsafe.com>
2021-04-19 07:47:32 +0100

    Added background image to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./Home.css";
+import NewsImg from "../assets/news.JPG";
 
-function Home() {
-  return <h1>Home</h1>;
-}
+export const Home = () => {
+  return (
+    <>
+      <h3>Home</h3>
+      <img className={styles.image} src={NewsImg} alt="News" />
+    </>
+  );
+};
 
 export default Home;
